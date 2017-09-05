316b1b62bcce9e4b6cee12b3646a6040d2c114a2
ikuto <ikuto0608@me.com>
2017-09-04 20:45:07 -0700

    coloured attachments for slack msg, add doting gem, modify logic

--- main.rb
+++ main.rb
@@ -9,7 +9,6 @@ require 'rufus-scheduler'
 require 'slack-ruby-client'
 require "redis"
 
-COINMARKET_URL = ENV['COINMARKET_URL']
 ATTTRIBUTES = ['currency-name', 'market-cap', 'price', 'volume', 'percent-24h']
 
 Slack.configure do |config|
@@ -20,7 +19,7 @@ end
 ## crawl coin market
 ######################
 def crawl_coin_market(unix_now)
-  coin_market = Nokogiri::HTML(open(COINMARKET_URL))
+  coin_market = Nokogiri::HTML(open(ENV['COINMARKET_URL']))
   rows = coin_market.xpath('//table/tbody/tr')
   rows.each do |row|
     coin_hash = ATTTRIBUTES.inject({}) do |hash, attribute|
@@ -40,6 +39,7 @@ end
 def up_down_check(unix_now)
   keys = redis.scan_each(:match => "*:#{unix_now}").to_a
   coin_names = keys.map{|key| key.chomp(":#{unix_now}") }
+  attachments = []
 
   coin_names.each do |coin_name|
     coin_history_keys = redis.scan_each(:match => "#{coin_name}:*").to_a
@@ -47,51 +47,70 @@ def up_down_check(unix_now)
     coin_histories = redis.mget(coin_history_keys).map {|coin_history| JSON.parse(coin_history) }
 
     latest_history = coin_histories[-1]
+    latest_price = latest_history['price'].dup
+    latest_price.slice!('$')
+    target_history = nil
+    target_price = ""
+    index = -1
+
     happening = coin_histories[0..-2].find do |c|
-      latest = latest_history['price']
-      latest.slice!('$')
-      current = c['price']
-      current.slice!('$')
-      !((latest.to_f / current.to_f) * 100).to_i.between?(95, 105)
+      index += 1
+      target_history = c
+      target_price = c['price'].dup
+      target_price.slice!('$')
+      !((latest_price.to_f / target_price.to_f) * 100).to_i.between?(95, 105)
     end
 
     if happening
-      client = Slack::Web::Client.new
-      client.chat_postMessage(
-        channel: ENV['HOGE_CHANNEL'],
-        as_user: true,
-        attachments: [{
-          pretext: "It's happening!",
-          title: "#{coin_name} has changed.",
-          color: 'good',
-          fields: [
-          {
-            title: "#{coin_name}",
-            value: "",
-            short: false
-          },
-          {
-            title: latest_history['price'],
-            value: Time.at(unix_now).to_datetime,
-            short: true
-          },
-          {
-            title: "",
-            value: "",
-            short: true
-          }
-				  ]
-        }]
-      )
+      parcentage = 100 - ((latest_price.to_f / target_price.to_f) * 100).to_i
+      parcentage *= -1
+      title = parcentage > 0 ? "is increasing." : "is reducing."
+      color = parcentage > 0 ? 'good' : 'danger'
+      attachments << {
+        title: "#{coin_name} " + title,
+        color: color,
+        fields: [
+        {
+          title: "Coin name: #{coin_name}",
+          value: "Change: #{parcentage}%",
+          short: false
+        },
+        {
+          title: "Price: #{target_history['price']}",
+          value: "Date: #{Time.at(/\d{10}/.match(coin_history_keys[index]).to_s.to_i).to_datetime}",
+          short: true
+        },
+        {
+          title: "Price: #{latest_history['price']}",
+          value: "Date: #{Time.at(unix_now).to_datetime}",
+          short: true
+        }
+        ]
+      }
     end
   end
+
+  return if attachments.empty?
+
+  client = Slack::Web::Client.new
+  client.chat_postMessage(
+    channel: "#coins_news",
+    as_user: true,
+    attachments: attachments
+  )
 end
 
+######################
+## get latest redis keys
+######################
 def latest_history_keys(coin_history_keys)
   latest_unixtimes = coin_history_keys.map {|coin_history_key| /\d{10}/.match(coin_history_key).to_s.to_i }.sort[-12..-1]
   latest_unixtimes.map {|unixtime| coin_history_keys.select {|coin_history_key| coin_history_key.include?(unixtime.to_s) } }.flatten
 end
 
+######################
+## redis instance
+######################
 def redis
   @redis || Redis.new
 end
