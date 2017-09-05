fa75e968eaecfb35def02a89c9be75e2e5957705
ikuto <ikuto0608@me.com>
2017-09-04 22:33:40 -0700

    set once to redid

--- main.rb
+++ main.rb
@@ -23,19 +23,20 @@ def crawl_coin_market(unix_now)
   coin_market = Nokogiri::HTML(open(ENV['COINMARKET_URL']))
   rows = coin_market.xpath('//table/tbody/tr')
 
-  redis.pipelined do
-    rows.each do |row|
-      coin_hash = ATTTRIBUTES.inject({}) do |hash, attribute|
-        hash.merge!(
-          { "#{attribute.gsub('-', '_')}".to_sym => row.at_css('.' + attribute).text.strip }
-        )
-        hash
-      end
+  coins_hash = {}
 
-      redis.set("#{coin_hash[:currency_name]}:#{unix_now}", coin_hash.to_json)
+  rows.each do |row|
+    coin_hash = ATTTRIBUTES.inject({}) do |hash, attribute|
+      hash.merge!(
+        { "#{attribute.gsub('-', '_')}".to_sym => row.at_css('.' + attribute).text.strip }
+      )
+      hash
     end
+
+    coins_hash.merge!({"#{coin_hash[:currency_name]}:#{unix_now}" => coin_hash.to_json})
   end
 
+  redis.mapped_mset(coins_hash)
 end
 
 ######################
